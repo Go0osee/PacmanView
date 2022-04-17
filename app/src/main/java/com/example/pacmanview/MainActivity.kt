76de88330a5a7935444577932ac64833e4f0fa3b
Go0osee <93539552+Go0osee@users.noreply.github.com>
2022-04-17 13:42:41 +0300

    pacman_done

--- app/src/main/java/com/example/pacmanview/MainActivity.kt
+++ app/src/main/java/com/example/pacmanview/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.pacmanview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.pacmanview.views.PacmanView
 
 class MainActivity : AppCompatActivity() {
+
+    private val btnStart by lazy { findViewById<Button>(R.id.btnStart) }
+    private val pacman by lazy { findViewById<PacmanView>(R.id.pacman) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        btnStart.setOnClickListener {
+            pacman.startAnimation()
+        }
     }
 }
